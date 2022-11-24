9098c8f337402f0c40f76e6283a0ebb20a2a74df
Lungu Marius <65728389+Lungumarius@users.noreply.github.com>
2022-11-24 09:52:03 +0100

    Add files via upload

---
+++ BankingAppServer/src/main/java/com/example/bankingappserver/BankingAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankingappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingAppServerApplication.class, args);
+    }
+
+}
---
+++ BankingAppServer/src/test/java/com/example/bankingappserver/BankingAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankingappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
