be39cf1c0315253f470fa56c1ebf9a48730b13ae
Lungu Marius <Marius.Lungu@solera.com>
2022-11-28 12:36:54 +0100

    Auth service Imp

--- BankingAppServer/src/main/java/com/example/bankingappserver/repository/CustomerRepository.java
+++ BankingAppServer/src/main/java/com/example/bankingappserver/repository/CustomerRepository.java
@@ -1,6 +1,5 @@
 package com.example.bankingappserver.repository;
 
-import com.example.bankingappserver.DTO.CustomerDTO;
 import com.example.bankingappserver.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Customer findCustomerByEmail(String email);
+    Customer findCustomerByUsername(String username);
 }
--- BankingAppServer/src/main/java/com/example/bankingappserver/services/implementation/AuthServiceImp.java
+++ BankingAppServer/src/main/java/com/example/bankingappserver/services/implementation/AuthServiceImp.java
@@ -1,7 +1,7 @@
 package com.example.bankingappserver.services.implementation;
 
 import com.example.bankingappserver.model.Customer;
-import com.example.bankingappserver.services.AuthService;
+import com.example.bankingappserver.repository.CustomerRepository;
 import com.example.bankingappserver.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -9,13 +9,20 @@ import java.util.Objects;
 
 
 public class AuthServiceImp implements AuthService {
+//    @Autowired
+//    Customer customer; //always gives null customer even with Autowired
+    // It is null cuz you are getting the model, not the repository where all the users are stored
     @Autowired
-    Customer customer; //always gives null customer even with Autowired
+    CustomerRepository customerRepository;
 
     //not working as intended
     @Override
-    public boolean authenticate(String username, String password) {  //returns true if parameters in this method match in customer list.
-        return Objects.equals(username, customer.getUsername()) && Objects.equals(password, customer.getPassword());
+    public boolean authenticate(String username, String password) {
+        //returns true if parameters in this method match in customer list.
+        //retrieve the customer based on the username given on the login page
+        Customer customer = customerRepository.findCustomerByUsername(username);
+        return Objects.equals(customer.getPassword(), password);
+//        return Objects.equals(username, customer.getUsername()) && Objects.equals(password, customer.getPassword());
     }
 
 
